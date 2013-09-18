81ed239ecae063b85fc98f9b027342f497f31bd6
rakshit-jain <er.rakshitjain@yahoo.com>
2013-09-18 18:06:15 +0530

    Initial checkin

---
+++ projectB/src/main/java/org/codehaus/openxma/mojo/App.java
@@ -0,0 +1,13 @@
+package org.codehaus.openxma.mojo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
